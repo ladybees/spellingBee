f3bf4403766f0c39aadf1349b34bf5bb1a2754da
L-Stewart <stewartalorin@gmail.com>
2019-04-16 13:03:18 -0700

    working on the dictionary loop

--- src/modules/dictionary.js
+++ src/modules/dictionary.js
@@ -11,8 +11,10 @@ const synthesize = require('./speechtext');
 // API Requests to obtain random list of words
 //============================================================
 const randomWord = require('random-words');
-const words = randomWord(3);
+const words = randomWord(5);
 const urls = [];
+const wordsAndSentence = [];
+const count = 5;
 
 //============================================================
 // API Requests to obtain word / sentence
@@ -24,46 +26,64 @@ const urls = [];
  * @returns {Array} of URLs that will be sent to Webster dictionary API
  */
 function makeURL(words){
+
   words.forEach(word => {
-
-    urls.push(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.COLLEGIATE_API_KEY}`);
-    });
+  // let urls = [];
+    urls.push(`https://dictionaryapi.com/api/v3/references/sd3/json/${word}?key=${process.env.INTERMEDIATE_API_KEY}`);
+  });
+  // return urls;
 }
-
 function getData(url){
+
   console.log(url);
+
   return superagent.get(url)
+
     .then(result => {
 
-      // H'Liana - need to find where in data we can access word + sentence
-      let sentence = result.body[[0][0]].def[0].sseq[0][0][1].dt[1][1][0].t; //PLACEHOLDER
-      let word = result.body[[0][0]].meta.id;
-      console.log('**' + word, sentence);
+      console.log(result.body[0].def[0].sseq.length);
 
-      return new Word(word, sentence);
+      for(let i = 0; i < result.body[0].def[0].sseq.length; i++) {
+
+        if (result.body[0].def[0].sseq[i][0][1].dt[1][1][0].t !== undefined) {
+          wordsAndSentence.push({
+            word: result.body[[0][0]].meta.id,
+            sentence: result.body[0].def[0].sseq[0][0][1].dt[1][1][0].t
+          });
+          console.log(wordsAndSentence);
+          break;
+        }
+        // let sentence = result.body[[0][0]].def[0].sseq[0][0][1].dt[1][1][0].t; //PLACEHOLDER
+        // let word = result.body[[0][0]].meta.id;
+        // console.log('**' + word, sentence);
+      }
+
     })
   .catch(err => console.error(err))
 }
 
-// H'Liana - Using Promise.all to make multiple API Requests to send word to Webster Dictionary API
 function speechToText(){
-  makeURL(words);
-  return Promise.all(urls.map(getData))
-    .then(result => {
-      // H'Liana - Should return array of objects (words and corresponding sentences)
-      // Then, we need to convert to speech + create mp3 files
-      result.forEach(word => {
-        let wordAudio = synthesize(word.word);
-        let sentAudio = synthesize(word.sentence);
+// H'Liana - Using Promise.all to make multiple API Requests to send word to Webster Dictionary API
 
-        result.wordFilePath = wordAudio;
-        result.sentenceFilePath = sentAudio;
-        result.wordReplacebyLine();
-      });
-      console.log(result)
-    });
+    makeURL(words);
+    return Promise.all(urls.map(getData))
+      // .then(result => {
+        // H'Liana - Should return array of objects (words and corresponding sentences)
+        // Then, we need to convert to speech + create mp3 files
+        // result.forEach(word => {
+          // let wordAudio = synthesize(word.word);
+          // let sentAudio = synthesize(word.sentence);
+
+          // result.wordFilePath = wordAudio;
+          // result.sentenceFilePath = sentAudio;
+          // result.wordReplacebyLine();
+        // });
+        // console.log(result)
+      // });
+
 }
-console.log(urls);
+
+
 speechToText();
 
 module.exports = {speechToText};
