e740fe6ce5ba295405c38c3c38c61f90676218bf
Hliana <liana.niehrah@gmail.com>
2019-04-15 19:41:52 -0700

    input

--- src/modules/wordObject.js
+++ src/modules/wordObject.js
@@ -1,20 +1,18 @@
-'use strict'
-
+'use strict';
 
 class WordObject{
 
-     constructor(word, sentence, audioFilePathWord, audioFilePathSentence){
-        this.word = word;
-        this.sentence = sentence;
-        this.audioFilePathSentence = audioFilePathWord;
-        this.audioFilePathSentence = audioFilePathSentence;
-    }
+   constructor(word, sentence, audioFilePathWord, audioFilePathSentence){
+    this.word = word;
+    this.sentence = sentence;
+    this.audioFilePathSentence = audioFilePathWord;
+    this.audioFilePathSentence = audioFilePathSentence;
+  }
 
+  wordReplacebyLine(){
+    this.sentence = this.sentence.replace(this.word, '______________');
+  }
 
-    wordReplacebyLine(){
-        this.sentence = this.sentence.replace(this.word, '______________');
-    }
 }
-
 
 module.exports = WordObject;
